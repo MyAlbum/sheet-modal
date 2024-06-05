8116b7049e4123a36b3d57eca46dd5cf1e5d04fc
Arjan Zuidema <flecks@gmail.com>
2024-06-05 11:09:31 +0200

    Eslint unused should warn, not error

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   extends: "@react-native",
   rules: {
     quotes: "off",
+    "@typescript-eslint/no-unused-vars": "warn",
     "react-native/no-inline-styles": "off",
   },
   parserOptions: {
