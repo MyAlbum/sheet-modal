c27d61bdc5aff6c0bfadf4bff9b5207f1c99106d
Arjan Zuidema <flecks@gmail.com>
2024-06-26 12:22:05 +0200

    FocusTrap initialFocus set to false

--- src/components/FocusTrap.tsx
+++ src/components/FocusTrap.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { default as _FocusTrap } from "focus-trap-react";
 
-export default function FocusTrap(props: _FocusTrap.Props) {
+export type FocusTrapProps = _FocusTrap.Props;
+export type FocusTrapOptions = _FocusTrap.Props["focusTrapOptions"];
+
+export default function FocusTrap(props: FocusTrapProps) {
   const { children, ...focusTrapOptions } = props;
 
   return <_FocusTrap {...focusTrapOptions}>{children}</_FocusTrap>;
