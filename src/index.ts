77cdf298cd5f66077f2f7ce285d1e1ed39791699
Clemens Mol <mail@clemensmol.nl>
2024-08-01 11:12:11 +0200

    export Portal

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 // Components
 export { default as SheetModal } from './components/Modal';
+export { default as Portal } from './components/Portal/Portal';
 export { default as PortalHost } from './components/Portal/PortalHost';
 export { default as SheetModalProvider } from './components/Provider';
 export { default as ScrollView } from './components/ScrollView';
